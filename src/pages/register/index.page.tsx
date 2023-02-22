58df6925316f4208fe26fe2ff78751716cce7c7f
Yago Neno <developer.neno@gmail.com>
2023-02-22 07:03:58 -0300

    feat: connection to the calendar

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -61,6 +61,8 @@ export default function Register() {
         username,
         name
       });
+
+      await router.push('/register/connect-calendar');
     } catch (error) {
       if (error instanceof AxiosError && error.response?.data?.message) {
         alert(error.response.data.message);
